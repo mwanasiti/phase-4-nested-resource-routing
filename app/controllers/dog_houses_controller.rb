7a9da811b857fb8ca01a87e03b8b53e6fb0b20ff
Mariam-Ali <ali.mariam.mwanasiti@gmail.com>
2022-10-04 16:49:42 -0400

    nested routing

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,15 +1,3 @@
-class DogHousesController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
-  def show
-    dog_house = DogHouse.find(params[:id])
-    render json: dog_house
-  end
-
-  private
-
-  def render_not_found_response
-    render json: { error: "Dog house not found" }, status: :not_found
-  end
-
+class DogHouse < ApplicationRecord
+  has_many :reviews, dependent: :destroy
 end
